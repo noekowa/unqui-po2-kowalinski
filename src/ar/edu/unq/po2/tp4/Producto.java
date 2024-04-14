fc4e6de9d7427199fb440fec4e0909fd21d3052f
Noelia <noeliakowalinski@gmail.com>
2024-04-14 20:02:59 -0300

    agrego tp4

---
+++ src/ar/edu/unq/po2/tp4/Producto.java
@@ -0,0 +1,48 @@
+package ar.edu.unq.po2.tp4;
+
+public class Producto {
+
+	private String nombre;
+	private Double precio;
+	private boolean esPrecioCuidado;
+	
+	public Producto(String nombre, Double precio, boolean esPrecioCuidado) {
+		super();
+		this.setNombre(nombre);
+		this.setPrecio(precio);
+		this.setEsPrecioCuidado(esPrecioCuidado);
+	}
+	
+	
+	public Producto(String nombre, Double precio) {
+		super();
+		this.setNombre(nombre);
+		this.setPrecio(precio);
+		this.setEsPrecioCuidado(false);
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	private void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public boolean esPrecioCuidado() {
+		return esPrecioCuidado;
+	}
+	private void setEsPrecioCuidado(boolean esPrecioCuidado) {
+		this.esPrecioCuidado = esPrecioCuidado;
+	}
+	public void aumentarPrecio(Double aumento) {
+		this.setPrecio(this.getPrecio()+ aumento);
+	}
+	
+
+}
---
+++ src/ar/edu/unq/po2/tp4/Supermercado.java
@@ -0,0 +1,47 @@
+package ar.edu.unq.po2.tp4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Supermercado {
+
+	private String nombre;
+	private String direccion;
+	private List<Producto> catalogo;
+	
+	public Supermercado(String nombre, String direccion) {
+		super();
+		this.setNombre(nombre);
+		this.setDireccion(direccion);
+		this.setCatalogo(new ArrayList<Producto>());
+		
+	}
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	private void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	private List<Producto> getCatalogo() {
+		return catalogo;
+	}
+	private void setCatalogo(List<Producto> catalogo) {
+		this.catalogo = catalogo;
+	}
+	public void agregarProducto(Producto prod) {
+		this.getCatalogo().add(prod);
+		
+	}
+	public Integer getCantidadDeProductos() {
+		return getCatalogo().size();
+	}
+	public Double getPrecioTotal() {
+		return getCatalogo().stream()
+				.mapToDouble(prod -> prod.getPrecio())
+				.sum();
+		
+	}
+	
+	
+}
