075ad651dc67dfdd7f7ea62a92f2738ccfc287a2
Noelia <noeliakowalinski@gmail.com>
2024-04-26 09:29:28 -0300

    tpsolid borrado

---
+++ test/ar/edu/unq/po2/tp8/PruebaTestSubclase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.tp8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PruebaTestSubclase extends PruebaTestSuperclase{
+
+	@Test
+	void testSubclase() {
+		fail("subclase test");
+	}
+
+}
---
+++ test/ar/edu/unq/po2/tp8/PruebaTestSuperclase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.tp8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PruebaTestSuperclase {
+
+	@Test
+	void testSuperclase() {
+		fail("superclase test");
+	}
+
+}
